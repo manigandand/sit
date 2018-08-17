748aeb2b00c824f05ac6d5f6a3be305225122e56
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2018-08-17 20:53:27 +0530

    pipfile change

--- sit/settings.py
+++ sit/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+PRE_REQ_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+ADDITIONAL_APPS = [
+    'django_extensions',
+]
+
+INSTALLED_APPS = PRE_REQ_APPS + ADDITIONAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
